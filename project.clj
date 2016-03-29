f1f91d7b171ac6b9a2b85d4d69ee5d5486312b4d
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-03-29 17:19:27 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject bitauth-cljs "0.3.2"
+  :description "ClojureScript Wrapper around BitPay's BitAuth"
+  :url "https://github.com/xcthulhu/bitauth-cljs"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"})
