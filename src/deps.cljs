249e99a07552dd73b68db412ee4766e5fce0200f
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-03-29 22:37:00 -0600

    No longer using capitalized 'BitAuth'

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,4 @@
 {:foreign-libs [{:file     "bitauth/bitauth.js"
                  :file-min "bitauth/bitauth.min.js"
-                 :provides ["com.bitpay.BitAuth"]}]
+                 :provides ["com.bitpay.bitauth"]}]
  :externs ["bitauth/externs/bitauth.js"]}
